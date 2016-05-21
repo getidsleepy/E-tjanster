08d32bb8f0fa49a0a2d258fb49af4a6b5be9aebd
Richard <heyrayray@Richards-MacBook-Air.local>
2016-05-21 12:34:49 +0200

    Lougout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
-
-//denna fil består endast av php och genomför en utloggning(förstör sessionen)
-
+	session_start();
+	session_destroy();
+	setcookie(session_name(), "", time() - 3600);
+	//denna fil består endast av php och genomför en utloggning(förstör sessionen)
+	header("location: login.html");
 ?>

697801a699402f4ace7bf6d2be0d78bc4e3ca2cc
Your Name <axel.wesstrom@gmail.com>
2016-05-22 12:43:19 +0200

    sökfunktion, lägg till annonser, visa annonser

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php
-
-//denna fil består endast av php och genomför en utloggning(förstör sessionen)
-
+	session_start();
+	session_destroy();
+	//denna fil består endast av php och genomför en utloggning(förstör sessionen)
+	header("location: login.html");
 ?>
